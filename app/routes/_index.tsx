8a2fc84602c2093b47c44d4b507f868a70319c5a
Simeon Griggs <simeon@hey.com>
2024-01-17 10:07:38 +0000

    first

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,7 +9,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div className="p-12 container mx-auto prose lg:prose-xl">
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
